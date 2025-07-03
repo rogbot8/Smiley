cde664d419b482d5e36e6778ec232e728f3298e0
rogbot8 <rkrakow@roughhouse.com>
2025-07-03 09:37:18 -0700

    Update smiley.js for hue changes

--- smiley.js
+++ smiley.js
@@ -32,25 +32,3 @@ drawFace(slider.value);
 slider.addEventListener('input', () => {
   drawFace(slider.value);
 });
-const canvas = document.getElementById('smiley');
-const ctx = canvas.getContext('2d');
-
-// Face
-ctx.beginPath();
-ctx.arc(150, 150, 100, 0, Math.PI * 2); // x, y, radius
-ctx.fillStyle = '#39FF14'; // a nice neon green
-
-ctx.fill();
-ctx.stroke();
-
-// Eyes
-ctx.beginPath();
-ctx.arc(110, 120, 10, 0, Math.PI * 2);
-ctx.arc(190, 120, 10, 0, Math.PI * 2);
-ctx.fillStyle = '#000';
-ctx.fill();
-
-// Smile
-ctx.beginPath();
-ctx.arc(150, 160, 50, 0, Math.PI); // Half-circle smile
-ctx.stroke();
